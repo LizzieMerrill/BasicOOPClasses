2c72edbc213c2946b073d8d930ba86ab635a8e7b
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-08-31 10:21:59 -0700

    Update Pet.java
    Finished Pet class

--- Pet.java
+++ Pet.java
@@ -1,7 +1,44 @@
-public class Pet{
-  private String name;
-  private int age;
-  private String location;
-  private String type;
-  
-}
+public class Pet {
+	  private String name;
+	  private int age;
+	  private String location;
+	  private String type;
+	  
+	  public Pet() {
+		  name = "";
+		  age = 0;
+		  location = "";
+		  type = "";
+	  }
+	  
+	  public Pet(String name, int age, String location, String type) {
+		  this.name = name;
+		  this.age = age;
+		  this.location = location;
+		  this.type = type;
+	  }
+	  
+	  public String getName() {
+		  return name;
+	  }
+	  
+	  public int getAge() {
+		  return age;
+	  }
+	  
+	  public String getType() {
+		  return type;
+	  }
+	  
+	  public void setName(String newName) {
+		  name = newName;
+	  }
+	  
+	  public void setAge(int newAge) {
+		  age = newAge;
+	  }
+	  
+	  public void setLocation(String newLocation) {
+		  location = newLocation;
+	  }
+}//end of Pet class
